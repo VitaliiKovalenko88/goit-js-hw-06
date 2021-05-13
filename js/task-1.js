8b1a133524b40d848f602e58e142c224fda633d8
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-05-13 02:06:48 +0200

    сделал первое задание.

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,9 @@
-const itemEl = document.querySelectorAll('.item');
-console.log(`В списке ${itemEl.length} категории.`);
+const itemsEl = document.querySelectorAll('.item');
+console.log(`В списке ${itemsEl.length} категории.`);
+
+itemsEl.forEach(item => {
+  const title = item.firstElementChild.textContent;
+  const number = item.lastElementChild.children.length;
+  console.log(`Категория: ${title} 
+  Количество елементов: ${number}`);
+});
