5222347973884bafc65618ee6ace3d0b14a299a2
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-10-13 21:00:56 +0200

    удалена лишняя ссылка на элемент и убран не нужный код

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,11 @@
 const refs = {
   input: document.querySelector('#name-input'),
-  titleLable: document.querySelector('body > h1'),
   titleName: document.querySelector('#name-output'),
 };
 
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  console.log(event.currentTarget.value.trim());
   refs.titleName.textContent =
     event.currentTarget.value.trim() !== ''
       ? event.currentTarget.value
