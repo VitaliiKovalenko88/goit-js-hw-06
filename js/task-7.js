fd5c8cce538af7edc924e1168f9f0150c42fc480
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-10-13 21:02:13 +0200

    закомментирован старый код с 32 потока

--- js/task-7.js
+++ js/task-7.js
@@ -1,10 +1,10 @@
-const refs = {
-  input: document.querySelector('#font-size-control'),
-  text: document.querySelector('#text'),
-};
+// const refs = {
+//   input: document.querySelector('#font-size-control'),
+//   text: document.querySelector('#text'),
+// };
 
-refs.input.addEventListener('input', onDraggingSlider);
+// refs.input.addEventListener('input', onDraggingSlider);
 
-function onDraggingSlider(event) {
-  refs.text.style.fontSize = event.currentTarget.value + 'px';
-}
+// function onDraggingSlider(event) {
+//   refs.text.style.fontSize = event.currentTarget.value + 'px';
+// }
