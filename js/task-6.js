47e6a9218ac8cd5cdd523ac5817cb1e0c6517592
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-10-13 21:01:47 +0200

    закомментирован старый код с 32 потока

--- js/task-6.js
+++ js/task-6.js
@@ -1,14 +1,14 @@
-const inputEl = document.querySelector('#validation-input');
+// const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener('blur', onBlurInput);
+// inputEl.addEventListener('blur', onBlurInput);
 
-function onBlurInput(event) {
-  const dataLength = inputEl.dataset.length;
-  if (event.currentTarget.value.length === parseInt(dataLength, 10)) {
-    inputEl.classList.add('valid');
-    inputEl.classList.remove('invalid');
-  } else {
-    inputEl.classList.add('invalid');
-    inputEl.classList.remove('valid');
-  }
-}
+// function onBlurInput(event) {
+//   const dataLength = inputEl.dataset.length;
+//   if (event.currentTarget.value.length === parseInt(dataLength, 10)) {
+//     inputEl.classList.add('valid');
+//     inputEl.classList.remove('invalid');
+//   } else {
+//     inputEl.classList.add('invalid');
+//     inputEl.classList.remove('valid');
+//   }
+// }
