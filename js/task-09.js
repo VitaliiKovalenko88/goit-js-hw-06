33d940d03f23a7f90a89b27d064224aa2562e37c
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-10-15 00:17:56 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,4 +1,20 @@
 const refs = {
-  container: document.querySelector('.widget'),
+  body: document.querySelector('body'),
   color: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
 };
+
+refs.button.addEventListener('click', onButtonClick);
+
+function onButtonClick(e) {
+  const {
+    color,
+    body: { style },
+  } = refs;
+  color.textContent = getRandomHexColor();
+  style.backgroundColor = color.textContent;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
